0e5b8ce07adab5e7bd3cd924f943973800beaceb
Jonathan Peraza Feliciano <jonathan.peraza@wizeline.com>
2020-08-24 19:13:37 -0500

    feat: move js files to ts and tsx and adding linting

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser', // the TypeScript parser we installed earlier
+  parserOptions: {
+    ecmaFeatures: { jsx: true } // Allows for the parsing of JSX
+  },
+  extends: [
+    'eslint:recommended', // eslint default rules
+    'plugin:@typescript-eslint/eslint-recommended', // eslint TypeScript rules (github.com/typescript-eslint/typescript-eslint)
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended', // eslint react rules (github.com/yannickcr/eslint-plugin-react)
+    "plugin:jsx-a11y/recommended", // accessibility plugin
+  ],
+  rules: {
+    'react/prop-types': 'off', // We turn off prop-types rule, as we will use TypeScript's types instead.
+  },
+};
