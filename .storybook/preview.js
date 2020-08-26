88924679fa9d9f230b7cfb4c519076f157f6abc5
Jonathan Peraza Feliciano <jonathan.peraza@wizeline.com>
2020-08-26 17:45:03 -0500

    feat: setup dark theme for storybook

---
+++ .storybook/preview.js
@@ -0,0 +1,7 @@
+// The preview application is essentially just your stories with 
+// a framework-agnostic 'router'. 
+// It renders whichever story the manager application tells it to render.
+// In this case we just use it to import the stylesheet and inject it 
+// in the context of our stories
+
+import "../styles/index.css";
