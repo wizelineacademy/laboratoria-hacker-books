64360d936efd3d74697c5ff1cde6be2afdea81e4
Tezzi Carbajal <tezzi.cah@gmail.com>
2020-11-10 23:48:29 -0600

    feat: link for redirect home on logo

--- components/navbar/navbar.tsx
+++ components/navbar/navbar.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Link from 'next/link'
 
 // Components
 import AvatarUser from '../svg/avatarUser'
@@ -6,9 +7,11 @@ import AvatarUser from '../svg/avatarUser'
 const Navbar = () => {
     return (
         <nav className="w-full flex items-center justify-between flex-wrap bg-blue-900 p-6" style={{ padding: "12px" }}>
+            <Link href="/" passHref>
             <div className="flex items-center flex-shrink-0 text-white mr-6">
                 <h2>Bookmark</h2>
             </div>
+            </Link>
             <div className="block lg:hidden">
                 <button className="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white">
                     <svg className="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" /></svg>
@@ -24,7 +27,7 @@ const Navbar = () => {
                         <AvatarUser />
                     </a>
                 </div>
-            </div>
+            </div>                  
         </nav>
     );
 }
