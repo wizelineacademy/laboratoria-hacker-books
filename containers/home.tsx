fdaec28f4d1b2527d26a2965c953fa772047a043
Tezzi Carbajal <tezzi.cah@gmail.com>
2020-11-18 13:35:07 -0600

    feat: 1 carousel rotate divs useEffect, conditions and 2 carousel scroll, overflow css, tailwind

--- containers/home.tsx
+++ containers/home.tsx
@@ -1,15 +1,20 @@
 import React, { useState, useEffect } from 'react';
-import Head from 'next/head'
+import Head from 'next/head';
 
-import ListCardVideo from '../components/listCardVideo/ListCardVideo'
-import ArrowPrevSVG from '../components/svg/arrowPrevSVG'
-import ArrowNextSVG from '../components/svg/arrowNextSVG'
+// Components
+import ListCardVideo from '../components/listCardVideo/listCardVideo';
+import ArrowPrevSVG from '../components/svg/arrowPrevSVG';
+import ArrowNextSVG from '../components/svg/arrowNextSVG';
 
-import styles from '../styles/Home.module.css'
+// Style
+import styles from '../styles/Home.module.css';
+interface iVideo {
+  cards?: [{ title: string; alt: any; src: any }];
+}
 
-export default function Home({ props }) {
-
-  const [viewNextVideo, setViewNextVideo] = useState(0)
+const Home: React.FC<iVideo> = () => {
+  const [viewNextVideo, setViewNextVideo] = useState(0);
+  const [showVideos, setShowVideos] = useState([]);
 
   const cards = [
     { title: 'Course 1', alt: 'img1', src: '1' },
@@ -18,85 +23,70 @@ export default function Home({ props }) {
     { title: 'Course 4', alt: 'img4', src: '4' },
     { title: 'Course 5', alt: 'img5', src: '5' },
     { title: 'Course 6', alt: 'img6', src: '6' },
-    { title: 'Course 7', alt: 'img7', src: '7' },
-    { title: 'Course 8', alt: 'img8', src: '8' },
-  ]
+  ];
+
+  useEffect(() => {
+    const videosToShow = [];
+
+    for (let i = viewNextVideo; i <= viewNextVideo + 3; i++) {
+      videosToShow.push(cards[i]);
+    }
+
+    setShowVideos(videosToShow);
+  }, [viewNextVideo]);
 
   const arrowNext = () => {
-    setViewNextVideo((viewNextVideo + 1) % cards.length);
-  }
+    if (viewNextVideo < 2) {
+      setViewNextVideo(viewNextVideo + 1);
+    }
+  };
 
   const arrowPrev = () => {
-    if (viewNextVideo == 0) {
-      setViewNextVideo(cards.length - 1)
-    } else {
-      setViewNextVideo(viewNextVideo - 1)
+    if (viewNextVideo > 0) {
+      setViewNextVideo(viewNextVideo - 1);
     }
-  }
+  };
 
   return (
     <div className={styles.container}>
       <Head>
         <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel='icon' href='/favicon.ico' />
       </Head>
 
       <main className={styles.main}>
-        <h1 className={`${styles.title} pb-6`}>
-          Welcome to Bookmark
-        </h1>
-
-        <div className="container  mx-auto overflow-hidden w-3/5" style={{ background: "orange" }}>
-          <ListCardVideo
-            cards={cards}
-          />
+        <h1 className={`${styles.title} pb-1`}>Welcome to Bookmark</h1>
+        <h3 className='p-3'>Category with button</h3>
+        <div className='container mx-auto overflow-hidden w-1/2 bg-orange-100'>
+          <ListCardVideo cards={showVideos} />
         </div>
-        <div className="flex flex-row">
-          <ArrowPrevSVG {...props} onClick={arrowPrev} />
-          <ArrowNextSVG {...props} onClick={arrowNext} />
+        <div className='flex flex-row'>
+          <ArrowPrevSVG onPrev={arrowPrev} />
+          <ArrowNextSVG onNext={arrowNext} />
         </div>
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+        <h3 className='p-3'>Category with scroll</h3>
+        <div className={`${styles.overflowList} container mx-auto overflow-hidden w-1/2 bg-gray-100`}>
+          <ListCardVideo cards={cards} />
         </div>
       </main>
 
       <footer className={styles.footer}>
         <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+          href='https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app'
+          target='_blank'
+          rel='noopener noreferrer'
         >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
+          Powered by
+          <img src='/vercel.svg' alt='Vercel Logo' className={styles.logo} />
         </a>
       </footer>
     </div>
-  )
-}
+  );
+};
+
+export default Home;
+
+//onClick?: () => void; //reload: (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void,
+//const arrowNext = (): void => {let next = setViewNextVideo((viewNextVideo + 1) % cards.length);};
+//const arrowPrev = (): void => {if (viewNextVideo == 0) {setViewNextVideo(cards.length - 1);} else {setViewNextVideo(viewNextVideo - 1);}};
