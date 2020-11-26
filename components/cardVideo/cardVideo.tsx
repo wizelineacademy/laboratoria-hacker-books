55f6454a49432dbdf965b4d4b551d66e770579d9
Tezzi Carbajal <tezzi.cah@gmail.com>
2020-11-26 12:05:21 -0600

    fix: destructured card for title

--- components/cardVideo/cardVideo.tsx
+++ components/cardVideo/cardVideo.tsx
@@ -4,14 +4,14 @@ import React from 'react';
 import ScoreStarSVG from '../svg/scoreStarSVG';
 import IconPlaySVG from '../svg/iconPlaySVG';
 
-const CardVideo = ({ card }) => {
+const CardVideo = ({ card: {title} }) => {
   return (
     <div className='max-w-sm rounded overflow-hidden shadow-lg w-3-5'>
       <div style={{ display: 'flex', justifyContent: 'center', padding: '4rem' }}>
         <IconPlaySVG />
       </div>
       <div className='px-6 py-4'>
-        <h2 className='font-bold text-xl mb-2'>{card.title}</h2>
+        <h2 className='font-bold text-xl mb-2'>{title}</h2>
       </div>
       <div className='mx-6 mt-2 mb-2 rounded-full bg-gray-200'>
         <span className='inline-block text-sm font-semibold text-gray-700 mr-2'>
