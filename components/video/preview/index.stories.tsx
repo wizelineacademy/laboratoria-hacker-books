aee234c40793dbe9fa87735cc996187b614ddef7
jonathanperazawl <jonathan.peraza@wizeline.com>
2020-12-01 18:52:56 -0600

    feat: add video preview component base

---
+++ components/video/preview/index.stories.tsx
@@ -0,0 +1,6 @@
+import * as React from 'react';
+import Preview from './index';
+
+export default { title: 'Video Preview' };
+
+export const simple = () => <Preview url="https://dummyimage.com/640x360/fff/aaa" />;
