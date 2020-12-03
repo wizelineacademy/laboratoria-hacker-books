d9510cab630f724b9bc74d5c9fdc948775c9fb1a
Tezzi Carbajal <tezzi.cah@gmail.com>
2020-12-03 13:30:20 -0600

    feat: base setup layout added in a new component for layout implementation testing

---
+++ components/sidebar/sidebar.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Sidebar = () => {
+  return (
+    <div className='float-left bg-white rounded-lg p-6 shadow-md w-56 h-screen'>
+      <h2>Hello I&apos;m a Sidebar ;D</h2>
+      <hr className='pt-8' />
+      <p>test</p>
+      <hr className='pt-8' />
+      <p>
+        Lorem ipsum dolor harum. Asperiores, ad odio debitis possimus veritatis
+        saepe! Dolore fuga velit exercitationem praesentium repudiandae
+        blanditiis eos error? Sunt, tempora minima!
+      </p>
+    </div>
+  );
+};
+
+export default Sidebar;
