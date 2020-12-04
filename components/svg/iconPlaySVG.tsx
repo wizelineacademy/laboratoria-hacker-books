fb0c8b1868c2cce73d7a5eb21649e8e2823737a5
Tezzi Carbajal <tezzi.cah@gmail.com>
2020-12-03 18:46:36 -0600

    feat: base styles test for card video in play btn

--- components/svg/iconPlaySVG.tsx
+++ components/svg/iconPlaySVG.tsx
@@ -4,7 +4,7 @@ interface iPlaySVG {
   size?: string;
 }
 
-const BtnPlaySVG: React.FC<iPlaySVG> = ({size = '34px'}) => {
+const BtnPlaySVG: React.FC<iPlaySVG> = ({size = '66px'}) => {
   return (
     <div>
       <svg
@@ -20,7 +20,7 @@ const BtnPlaySVG: React.FC<iPlaySVG> = ({size = '34px'}) => {
         width={size}
       >
         <path
-          fill='currentColor'
+          fill='white'
           d='M371.7 238l-176-107c-15.8-8.8-35.7 2.5-35.7 21v208c0 18.4 19.8 29.8 35.7 21l176-101c16.4-9.1 16.4-32.8 0-42zM504 256C504 119 393 8 256 8S8 119 8 256s111 248 248 248 248-111 248-248zm-448 0c0-110.5 89.5-200 200-200s200 89.5 200 200-89.5 200-200 200S56 366.5 56 256z'
         ></path>
       </svg>
