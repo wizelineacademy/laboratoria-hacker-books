f5a30f275b4360e45e8f94511efb41936fb7786d
Tezzi Carbajal <tezzi.cah@gmail.com>
2020-12-03 18:44:54 -0600

    feat: added tag image for show according to the proto and styles in the simulate btn play

--- components/cardVideo/cardVideo.tsx
+++ components/cardVideo/cardVideo.tsx
@@ -4,6 +4,9 @@ import React from 'react';
 import ScoreStarSVG from '../svg/scoreStarSVG';
 import IconPlaySVG from '../svg/iconPlaySVG';
 
+// Styles
+import styles from './cardVideo.module.css'
+
 interface iCard {
   title: string,
   alt?: string,
@@ -22,14 +25,17 @@ type Props = iCard | iPlaySVG | iScoreSVG;
 
   const CardVideo: React.FC<Props> = props => {
 
-    const { title } = props as iCard;
+    const { title, src } = props as iCard;
     const { size } = props as iPlaySVG;
     const { sizeScore } = props as iScoreSVG;
 
   return (
-    <div className='max-w-sm rounded overflow-hidden shadow-lg w-3-5'>
-      <div  className="flex justify-center p-12">
+    <div className='max-w-sm rounded overflow-hidden shadow-lg'>
+      <div  className="flex justify-center">
+        <img src={src} alt="course" style={{height: '188px'}}/>
+         <div  className={`${styles.mtPlay} absolute`}>
         <IconPlaySVG size={size} />
+         </div>
       </div>
       <div className='px-6 py-4'>
         <h2 className='font-bold text-xl mb-2'>{title}</h2>
