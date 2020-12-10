168583101297d353f817da9e989e57d94dabb372
jonathanperazawl <jonathan.peraza@wizeline.com>
2020-12-10 13:53:38 -0600

    feat: add image component

---
+++ components/image/index.stories.tsx
@@ -0,0 +1,6 @@
+import * as React from 'react';
+import Image from './index';
+
+export default { title: 'Image' };
+
+export const image = () => <Image src="https://dummyimage.com/640x360/fff/aaa" alt="" />
