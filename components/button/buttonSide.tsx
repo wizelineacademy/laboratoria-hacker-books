1550d6ad908e6a99d783a9f1ce95a6bbfbafbd10
Teresa Carbajal <tezzi.cah@gmail.com>
2021-01-19 00:23:49 -0600

    feat: create base buttons for sidebar

---
+++ components/button/buttonSide.tsx
@@ -0,0 +1,12 @@
+
+export interface Props {
+  Prop: Props;  
+}
+ 
+const ButtonSide : React.FC<Props> = () => {
+    return ( 
+        <button className="bg-transparent w-full hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">Favorite</button>
+     );
+}
+ 
+export default ButtonSide;
