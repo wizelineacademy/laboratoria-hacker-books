de4cfa69f5d704558c64b2b5c19487698cb2c68b
Teresa Carbajal <tezzi.cah@gmail.com>
2021-01-19 00:40:45 -0600

    feat: create for interface base buttons for sidebar

--- components/button/buttonSide.tsx
+++ components/button/buttonSide.tsx
@@ -1,9 +1,9 @@
 
 export interface Props {
-  Prop: Props;  
+  props: string;  
 }
  
-const ButtonSide : React.FC<Props> = () => {
+const ButtonSide : React.FC<Props> = ({props}) => {
     return ( 
         <button className="bg-transparent w-full hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">Favorite</button>
      );
