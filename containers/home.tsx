6b04bbd7067e49df1e09cae9f018129f11dce87a
Teresa Carbajal <tezzi.cah@gmail.com>
2021-01-18 23:30:00 -0600

    feat: layout for componenet home detect div space

--- containers/home.tsx
+++ containers/home.tsx
@@ -1,52 +1,54 @@
-import React from 'react';
-import Head from 'next/head';
+import React from "react";
+import Head from "next/head";
 
 // Styles
-import styles from '../styles/Home.module.css';
+import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
-    <div className='pt-6'>
+    <div className="pt-6">
       <Head>
         <title>Create Next App</title>
-        <link rel='icon' href='/favicon.ico' />
+        <link rel="icon" href="/favicon.ico" />
       </Head>
       <h1 className={styles.title}>Welcome to Next.js!</h1>
 
       <div className={styles.main}>
-      <p className={styles.description}>
-        Get started by editing
-        <code className={styles.code}>pages/index.js</code>
-      </p>
+        <p className={styles.description}>
+          Get started by editing
+          <code className={styles.code}>pages/index.js</code>
+        </p>
 
-      <div className={styles.grid}>
-        <a href='https://nextjs.org/docs' className={styles.card}>
-          <h3>Documentation &rarr;</h3>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
+        <div className={styles.grid}>
+          <a href="https://nextjs.org/docs" className={styles.card}>
+            <h3>Documentation &rarr;</h3>
+            <p>Find in-depth information about Next.js features and API.</p>
+          </a>
 
-        <a href='https://nextjs.org/learn' className={styles.card}>
-          <h3>Learn &rarr;</h3>
-          <p>Learn about Next.js in an interactive course with quizzes!</p>
-        </a>
+          <a href="https://nextjs.org/learn" className={styles.card}>
+            <h3>Learn &rarr;</h3>
+            <p>Learn about Next.js in an interactive course with quizzes!</p>
+          </a>
 
-        <a
-          href='https://github.com/vercel/next.js/tree/master/examples'
-          className={styles.card}
-        >
-          <h3>Examples &rarr;</h3>
-          <p>Discover and deploy boilerplate example Next.js projects.</p>
-        </a>
+          <a
+            href="https://github.com/vercel/next.js/tree/master/examples"
+            className={styles.card}
+          >
+            <h3>Examples &rarr;</h3>
+            <p>Discover and deploy boilerplate example Next.js projects.</p>
+          </a>
 
-        <a
-          href='https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app'
-          className={styles.card}
-        >
-          <h3>Deploy &rarr;</h3>
-          <p>Instantly deploy your Next.js site to a public URL with Vercel.</p>
-        </a>
+          <a
+            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+            className={styles.card}
+          >
+            <h3>Deploy &rarr;</h3>
+            <p>
+              Instantly deploy your Next.js site to a public URL with Vercel.
+            </p>
+          </a>
+        </div>
       </div>
-    </div>
     </div>
   );
 }
